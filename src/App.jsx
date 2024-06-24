8b8df2b7a25783eb683847add267f8fc61ef926f
Jaykishor <Jaykishorkushwaha009@gmail.com>
2024-06-24 18:09:42 +0530

    Lading-Page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Analystic from "./components/Analystic"
+import Newsletter from "./components/Newsletter"
+import Cards from './components/Cards'
+import Footer from "./components/Footer"
+const App = () => {
+  return (
+   <div>
+
+   <Navbar/>
+   <Hero/>
+ <Analystic/>
+ <Newsletter/>
+ <Cards/>
+<Footer/>
+   </div>
+  )
+}
+
+export default App
